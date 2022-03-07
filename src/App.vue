2012505d39a8c874659d42641d68c5d57ae78873
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-03-07 08:47:54 +0100

    use props Parent child

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,70 @@
 <template>
-  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App" url="you code "/> -->
-  <!-- <h1> hi {{add()}}</h1> -->
-  <!-- <h2 v-html="greate"></h2> -->
-  <h1> {{fullname}}</h1>
-  <h1> convert MAD USD</h1>
-  <label >Hour</label>
-  <input type="number"   v-model="h">
-  <label >minute</label>
-  <input type="number"   v-model="m">
-  <label >second</label>
-  <input type="number"   v-model="s">
+  <div>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App" url="you code "/> -->
+    <!-- <h1> hi {{add()}}</h1> -->
+    <!-- <h2 v-html="greate"></h2> -->
+    <!-- <h1>{{ fullname }}</h1>
+    <h1>convert MAD USD</h1>
+    <label>Hour</label>
+    <input type="number" v-model="h" />
+    <label>minute</label>
+    <input type="number" v-model="m" />
+    <label>second</label>
+    <input type="number" v-model="s" /> -->
+    <Greet :name="first" heroName="batman" />
+    <Greet name="abdo" heroName="batman1"/>
+    <Greet name="zaynab" heroName="batman2" />
+    
+  </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import Greet from "./components/Greet.vue";
+// import HelloWorld from './components/HelloWorld.vue';
+// // import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  data(){
-    return{
-     first:'youssef',
-     last:'elkafhi',
-    h :0,
-    m:0,
-    s:0,
+  components: { Greet },
+  name: "App",
 
-    }
+  data() {
+    return {
+      first: "youssef",
+      last: "elkafhi",
+      h: 0,
+      m: 0,
+      s: 0,
+    };
   },
-  methods:{
-    add(){
-      return 1+2;
-    }
-  },
-  computed:{
-    fullname(){
-      return `${this.first}`
-    }
-  },
-  watch:{
-    h(v){
-      this.m = v*60;
-      this.s = v*60*60;
-    }, 
-    m(v){
-      this.h = v/60;
-      this.s = v*60;
+  methods: {
+    add() {
+      return 1 + 2;
     },
-     s(v){
-      this.m = v/60;
-      this.h = v/3600;
+  },
+  computed: {
+    fullname() {
+      return `${this.first}`;
     },
-    
-  }
-}
+  },
+  watch: {
+    h(v) {
+      this.m = v * 60;
+      console.log("run hour");
+      // this.s = v*60*60;
+    },
+    m(v) {
+      // this.h = v/60;
+      this.s = v * 60;
+      this.h = v / 60;
+      console.log("run min");
+    },
+    s(v) {
+      this.m = v / 60;
+      console.log("run sec");
+    },
+  },
+};
 </script>
 
 <style>
