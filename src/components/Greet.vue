d726612e0db23a96a15210ee1fbff9eef07e7350
Elkafhi-Youssef <elkafhiyoussef@gmail.com>
2022-03-07 08:47:26 +0100

    Create new Component

---
+++ src/components/Greet.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+<h2>hi {{name}} : {{heroName}}</h2>
+  </div>
+</template>
+
+<script>
+  export default {
+    name:'Greet-Com',
+    props:['name','heroName']
+  }
+</script>
+
+<style scoped>
+
+</style>
